5171798a9d1eb78fc1d424838ae928f00c25f52e
Lucas Pitol <lucascpitol0@gmail.com>
2026-01-29 19:44:07 -0300

    expose colaborador edit and delete in gateway

--- back-end/interface/ColaboradoresGateway.ts
+++ back-end/interface/ColaboradoresGateway.ts
@@ -1,10 +1,17 @@
-import type { ColaboradorDTO, CriarColaboradorDTO } from '../domain/types/ColaboradorDTO';
+import type {
+  ColaboradorDTO,
+  CriarColaboradorDTO,
+  AtualizarColaboradorDTO,
+} from '../domain/types/ColaboradorDTO';
 import type { ColaboradorRepository } from '../domain/repositories/ColaboradorRepository';
 import { colaboradorRepositoryFirestore } from '../data/firebase/colaborador.collection';
 
 export interface ColaboradoresGateway {
   listar(): Promise<ColaboradorDTO[]>;
   criar(dto: CriarColaboradorDTO): Promise<ColaboradorDTO>;
+  editar(id: string, dto: AtualizarColaboradorDTO): Promise<ColaboradorDTO>;
+  excluir(id: string): Promise<void>;
+  excluirEmMassa(ids: string[]): Promise<void>;
 }
 
 class ColaboradoresGatewayImpl implements ColaboradoresGateway {
@@ -16,6 +23,18 @@ class ColaboradoresGatewayImpl implements ColaboradoresGateway {
 
   async criar(dto: CriarColaboradorDTO): Promise<ColaboradorDTO> {
     return this.repository.criar(dto);
+  }
+
+  async editar(id: string, dto: AtualizarColaboradorDTO): Promise<ColaboradorDTO> {
+    return this.repository.atualizar(id, dto);
+  }
+
+  async excluir(id: string): Promise<void> {
+    return this.repository.remover(id);
+  }
+
+  async excluirEmMassa(ids: string[]): Promise<void> {
+    return this.repository.removerEmMassa(ids);
   }
 }
 
